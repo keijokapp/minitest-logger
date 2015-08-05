3e0f4f9009e10f4b73eff5fbc34bf36697df22d2
keijokapp <keijo.kapp@gmail.com>
2015-08-05 08:04:25 +0300

    ?

--- common/BalticContest.js
+++ common/BalticContest.js
@@ -4,17 +4,17 @@ import CallUtil from './CallUtil';
 var BalticContest = Contest.extend({}, {
 	period: { start: 0, end: 0 },
 	sections: {
-		A: SectionPreset.SingleMixed,
-		B: SectionPreset.SingleCW,
-		C: SectionPreset.SingleSSB,
-		D: 'single operator, 2 hours, CW/SSB',
-		E: SectionPreset.MultiSingleMixed,
-		F: 'SWL'
+		'A': SectionPreset.SingleMixed,
+		'B': SectionPreset.SingleCW,
+		'C': SectionPreset.SingleSSB,
+		'D': 'single operator, 2 hours, CW/SSB',
+		'E': SectionPreset.MultiSingleMixed,
+		'F': 'SWL'
 	},
 	frequencies: [ [3510, 3600], [ 3600, 3750 ] ],
 	exchange: [ 'RST', 'NR' ],
 	score: function(qso) {
-		if()
+		if(CallUtil.)
 	}
 	
 })
